2d4190577e7c860db9df55786e123490e45fd3b7
Alexey Zakruzhetskyi <l500etc@gmail.com>
2020-03-02 00:19:09 +0200

    add versioning, add registrations_controller.rb

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'puma', '~> 4.1'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'rack-cors'
+
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
 gem 'simple_command'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'authenticate', to: 'authentication#authenticate'
+  concern :api_base do
+    post 'authenticate', to: 'authentication#authenticate'
+    post 'register', to: 'registrations#create'
+  end
+
+  namespace :v1 do
+    concerns :api_base
+  end
 end
